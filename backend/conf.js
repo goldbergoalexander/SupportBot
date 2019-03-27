acc0a8910ebbdb593f84a64e3bc705bfc86c7554
Alexandergoldberg <alexandergoldberg@ukr.net>
2019-03-27 09:28:01 +0200

    update

--- backend/conf.js
+++ backend/conf.js
@@ -3,7 +3,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '674962170:AAHUr6Fy15a47n2-GLLstH96TawRDq5xpmU';
+const token = bot_token;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
